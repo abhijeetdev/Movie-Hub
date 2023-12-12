7ad51d09c62203cdd53d605c869f0a880b1f9725
Abhijeet Banarases <abhijeet.idev@gmail.com>
2023-12-12 18:21:49 +0000

    Fixed Layout constraints warning at runtime

--- MovieHub/Screens/Home/Views/Cells/PeopleCell.swift
+++ MovieHub/Screens/Home/Views/Cells/PeopleCell.swift
@@ -14,15 +14,12 @@ final class PeopleCell: UICollectionViewCell {
     }
     
     @IBOutlet weak var imageView: RoundedImageView!
-    @IBOutlet weak var titleLabel: UILabel!
     
     var viewModel: PeopleCellViewModel? {
         didSet {
             guard let viewModel = viewModel else {
                 return
             }
-            
-            titleLabel.text = viewModel.name
             
             Task {
                 do {
@@ -35,40 +32,6 @@ final class PeopleCell: UICollectionViewCell {
             }
         }
     }
-    
-//    func setup(item: People) {
-//        textLabel.text = item.name
-//        imageUrlString = item.profilePath
-//    }
-//    
-//    var imageUrlString: String? {
-//        didSet {
-//            guard let urlsString = imageUrlString else {
-//                return
-//            }
-//            let imageUrl = URL(string: "\(URLs.ImageRequest.w185)/\(urlsString)")
-//            fetchImage(imageUrl: imageUrl) { [weak self] result in
-//                switch result {
-//                case .success(let image):
-//                    DispatchQueue.main.async {
-//                        self?.imageView.image = image
-//                        self?.imageView.contentMode = .scaleToFill
-//                    }
-//                case .failure:
-//                    break
-//                }
-//            }
-//        }
-//    }
-//    
-//    public func fetchImage(imageUrl: URL?, completion: @escaping (Result<UIImage, Error>) -> Void) {
-//        guard let imageUrl = imageUrl else {
-//            completion(.failure(URLError(.badURL)))
-//            return
-//        }
-//        
-//        ImageLoader.shared.downloadImage(imageUrl, completion: completion)
-//    }
 }
 
 final class RoundedImageView: UIImageView {
